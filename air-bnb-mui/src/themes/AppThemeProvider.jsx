d4fe44a411e9d9203073ae176798425d33949975
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-03-20 22:42:57 -0700

    Header

--- air-bnb-mui/src/themes/AppThemeProvider.jsx
+++ air-bnb-mui/src/themes/AppThemeProvider.jsx
@@ -4,8 +4,66 @@ const theme = createTheme({
     typography: {
         allVariants: {
             fontFamily: "Raleway",
+            textTransform: "none",
+            fontSize: 15,
         }
+    },
+    palette: {
+      primary: {
+        main: grey[700]
+      },
+      secondary: {
+        main: pink[500]
+      }
+    },
+    components : {
+      MuiTypography : { // pass default props to mui Typography component
+        defaultProps: {
+          sx:{
+            px: 1
+          },
+          variant: "subtitle2",
+          textTransform: 'capitalize',
+        }
+      },
+      MuiStack : {
+        defaultProps:{
+          sx:{
+            px: 2,
+            py: 1
+          },
+          spacing:2,
+          direction: 'row'
+        }
+      },
+      MuiPaper: {
+        defaultProps: {
+          elevation:0
+        }
+      },
+      MuiLink:{
+        defaultProps:{
+          sx:{
+            color: theme => theme.palette.primary.main
+          },
+          underline: "none"
+        }
+      },
+      MuiButton: {
+        defaultProps:{
+          size: "small",
+          p:0,
+          disableRipple: true
+        },
+        variant: 'text'
+      },
+      MuiTab : {
+         defaultProps:{
+          disableRipple: true,
+         }
+      }
     }
+
 });
 
 const AppThemeProvider = (prop) => {
